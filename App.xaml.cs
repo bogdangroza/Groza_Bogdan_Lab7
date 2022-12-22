d662f19a8128cf9dcb598f433b92645c5482d556
bogdangroza <grozab92@gmail.com>
2022-12-22 19:59:35 +0200

    lab9

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,30 @@
-﻿namespace Groza_Bogdan_Lab7;
+﻿using Groza_Bogdan_Lab7.Data;
+
+namespace Groza_Bogdan_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
 		MainPage = new AppShell();
 	}
+
+    
 }
